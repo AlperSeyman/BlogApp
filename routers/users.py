7246989ba16cf9610d627a4fef81cfe681139a0c
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-02-19 18:01:20 +0100

    updated and improved

--- routers/users.py
+++ routers/users.py
@@ -45,7 +45,11 @@ async def get_user_posts_api(user_id: int, db: Annotated[AsyncSession, Depends(g
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
-    result = await db.execute(select(models.Post).options(selectinload(models.Post.author)).where(models.Post.user_id == user_id))
+    result = await db.execute(
+        select(models.Post)
+        .options(selectinload(models.Post.author))
+        .where(models.Post.user_id == user_id)
+        .order_by(models.Post.date_posted.desc()))
     posts = result.scalars().all()
     return posts
 
